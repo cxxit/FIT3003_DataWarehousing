b1098912330a3440b348eaf895d1d81d3f3fe609
Chin Xu Xin <xchi0019@student.monash.edu>
2026-09-02 17:18:07 +0800

    feat: add Part B: Data exploration and identifying dirty data in operational database datasets

--- revision/lab3a.sql
+++ revision/lab3a.sql
@@ -109,5 +109,137 @@ group by semid, timeid, class_id, major_code;
 
 select * from fact_uselog;
 
+-- PART B: Data Exploration 
+-- How many rows in ‘dw.student’?  (CHECKED--CORRECT)
+select count(*) as number_of_rows 
+from dw.student;
+
+-- What does ‘dw.student’ look like? Write a SQL query 
+-- to retrieve and review all columns and rows. (CHECKED--CORRECT)
+select * from dw.student;
+
+-- How many rows in ‘dw.uselog’? (CHECKED--CORRECT)
+select count(*) as number_of_rows 
+from dw.uselog;
+
+-- What does ‘dw.uselog’ look like?  (CHECKED--CORRECT)
+select * from dw.uselog;
+select to_char(log_date, 'DD-MON-YYYY') as log_date, to_char(log_time, 'HH24:MI') as log_time, student_id, act
+from dw.uselog;
+
+-- How many rows in ‘dw.major’?  (CHECKED--CORRECT)
+select count(*) as number_of_rows 
+from dw.major;
+
+
+-- What does ‘dw.major’ look like?  (CHECKED--CORRECT)
+select * from dw.major;
+
+
+-- How many rows in ‘dw.class’?  (CHECKED--CORRECT)
+select count(*) as number_of_rows 
+from dw.class;
+
+
+-- What does ‘dw.class’ look like?
+select * from dw.class;
+
+-- We've explored all the tables in the data warehouse earlier. 
+-- Does anything seem wrong?
+select count(*) from fact_uselog;
+
+select count(*) from tempfact_uselog;
+
+-- fact_uselog is the aggregated version of tempfact_uselog
+-- aggregate function count(nstudent_id) ised om fact_uselog groups all student_id columns which are 
+-- the same together
+-- thus, reducing the number of rows
+
+-- fact_uselog would serve as a fact table, where the aggregated data is stored and used for analysis 
+
+
+-- the relationship between dw.student and dw.uselog is one to many 
+-- each student has at least one instance of logging in or many instances 
+
+select log_date, to_char(log_time,'HH24:MI') as log_time, student_id, class_id, major_code, semid, timeid 
+from tempfact_uselog;
+select * from dw.uselog;
+-- given that temfact uselog has an instance of uselog record for each student each time they are logged in 
+-- shouldnt that mean the number of rows in uselog = number of rows in tempfact_uselog
+
+-- NUMBER OF ROWS IN TEMPFACT_USELOG 
+select count(*) number_of_rows
+from tempfact_uselog; -- 170610 
+
+
+-- NUMBER OF ROWS IN USELOG 
+select count(*) number_of_rows 
+from dw.uselog; -- 108267 
+
+-- the number of records does not match, that means there may be issues with the data 
+-- code use to create the tempfact_uselog:
+
+-- create table tempfact_uselog as
+-- select U.log_date , U.log_time,
+-- U.student_ID, S.class_id, S.major_code
+-- from dw.uselog U, dw.student S
+-- where U.student_id = S.student_id;
+
+
+-- tempfact_uselog uses both the table uselog and student
+-- so let's check both of those table
+select * from dw.student
+order by student_id;
+
+select student_id,count(student_id) number_repeated
+from dw.student 
+group by student_id
+order by student_id;
+
+-- from this code we can see that there is duplication of the student record in dw.student_id when there shouldnt be 
+-- as student_id should represent the primary key 
+-- and that each student only should have one student_id 
+-- duplication could have cause what happened in tempfact_uselog 
+-- there is dirty data originating from the dw.student table
+
+select * from dw.uselog;
+-- check for duplication of data in dw.uselog? 
+select log_date, to_char(log_time,'HH24:MI'), student_id, act, count(*)
+from dw.uselog
+group by log_date, to_char(log_time,'HH24:MI'), student_id, act
+having count(*) >1;
+
+-- and this suggests that dw.uselog also has duplicated data 
+select * from dw.uselog 
+where student_id not in (
+    select student_id from dw.student
+); -- none 
+-- thus all student recorded in uselog table is in the student table
+
+select * from dw.student;
+-- check if there is illegal class_id, and major_code in the student table 
+
+select * from dw.student 
+where class_id not in ( 
+    select class_id from dw.class
+); -- none 
+
+select count(*) as count 
+from (
+select * from dw.student 
+where major_code not in (
+    select major_code from dw.major
+)); -- 122 majors in student table is not listed in the major table
+select * 
+from dw.uselog, dw.student
+where dw.uselog.student_id = dw.student.student_id
+and dw.student.major_code NOT IN 
+   (select major_code from dw.major);
+
+
+
+
+
+
 
 
